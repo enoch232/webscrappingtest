68862c4c841385a9915f6c4e07f7d17a43b2c646
enoch232 <enoch232@gmail.com>
2016-03-26 22:46:57 -0700

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+  def index
+	  	#! /usr/bin/env ruby
+
+	require 'nokogiri'
+	require 'open-uri'
+
+	# Fetch and parse HTML document
+	url = 'http://www.ebay.com/sch/i.html?_odkw=galaxy+s7&_osacat=0&_from=R40&_trksid=p2045573.m570.l1313.TR3.TRC2.A0.H0.Xgalaxy+s7+edge.TRS0&_nkw=galaxy+s7+edge&_sacat=0'
+	doc = Nokogiri::HTML(open(url))
+
+	doc.css('.li').each do |content|
+		@titles ||= ""
+		@titles = @titles +"\n"+ content.at_css('.vip, strong').text
+
+	end
+	@titles = @titles.split("\n")
+  end
+end
